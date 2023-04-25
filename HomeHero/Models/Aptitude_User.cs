2d0bb544ab11d5d7d500439e8e8c35cdf1716621
JOSÉ MIGUEL TUOZZO IZAGUIRRE <jtuozzo@ucundinamarca.edu.co>
2023-04-25 17:13:12 -0500

    Correction first table

--- HomeHero/Models/Aptitude_User.cs
+++ HomeHero/Models/Aptitude_User.cs
@@ -1,6 +1,18 @@
-﻿namespace HomeHero.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeHero.Models
 {
     public class Aptitude_User
     {
+       
+		[Key]
+        public int userApID { get; set; }
+
+        [ForeignKey("")]
+        public int aptitudeID { get; set; }
+        [ForeignKey("")]
+        public int userID { get; set; }
+
     }
 }
